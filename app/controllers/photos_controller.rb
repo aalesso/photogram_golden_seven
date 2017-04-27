875e52c6d084ed48ba88b549bf819dbf5f5a8c91
aalesso <aalesso@users.noreply.github.com>
2017-04-27 16:31:48 -0500

    last3

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,9 +9,10 @@ class PhotosController < ApplicationController
     cap = params[:the_caption]
     new_photo = Photo.new
     new_photo.source = url
-    new_photo.caption = url 
+    new_photo.caption = cap
     new_photo.save
-    render("photos/create_row.html.erb")
+    # render("photos/create_row.html.erb") WE DONT USE RENDER, BECAUSE WE DONT WANT TO RENDER A NEW HTML, BUT RE DIRECT TO ANOTHER ACTION
+    redirect_to("/photos/#{new_photo.id}")
   end
 
   def index
@@ -29,6 +30,14 @@ class PhotosController < ApplicationController
   end
 
   def edit_form
+    # @the_id=params[:id]
+    the_id=params[:id]
+    @id_to_edit = the_id
+    @my_photo=Photo.find(the_id)
+    new_caption=params[:the_caption2]
+    new_source=params[:the_source2]
+    
+
     render("photos/edit_form.html.erb")
   end
 
