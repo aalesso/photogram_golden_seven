7e91bcbbdfd82af96a106ae6e518ef52edafcee9
aalesso <aalesso@users.noreply.github.com>
2017-04-29 14:49:34 -0500

    last2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -36,13 +36,22 @@ class PhotosController < ApplicationController
     @my_photo=Photo.find(the_id)
     new_caption=params[:the_caption2]
     new_source=params[:the_source2]
-    
+
 
     render("photos/edit_form.html.erb")
   end
 
   def update_row
-    render("photos/update_row.html.erb")
+    the_id=params[:id]
+    url2 = params[:the_source2]
+    cap2 = params[:the_caption2]
+    edited_photo = Photo.find(the_id)
+    edited_photo.source = url2
+    edited_photo.caption = cap2
+    edited_photo.save
+    # render("photos/create_row.html.erb") WE DONT USE RENDER, BECAUSE WE DONT WANT TO RENDER A NEW HTML, BUT RE DIRECT TO ANOTHER ACTION
+    redirect_to("/photos/#{edited_photo.id}")
+
   end
 
   def destroy_row
